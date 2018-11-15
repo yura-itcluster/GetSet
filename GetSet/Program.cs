9a48c0249e78550603b7c326dcd13f5565e9e27c
yuriy.maturin <yuriy.maturin@gmail.com>
2018-11-15 23:03:57 +0200

    Add project files.

---
+++ GetSet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSet
+{
+    class Person
+    {
+        private int age;
+
+        public int Age
+        {
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Возраст должен быть больше 17");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+            get { return age; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 5;
+            Console.WriteLine(person.Age);
+            Console.ReadKey();
+        }
+    }
+}
